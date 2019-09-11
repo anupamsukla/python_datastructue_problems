ca7f997ac4dd922df155a25f59c06cdd45e313bf
anupamsukla <32832216+anupamsukla@users.noreply.github.com>
2019-09-12 03:03:35 +0530

    Update bubblesort.py

--- sorting/bubblesort.py
+++ sorting/bubblesort.py
@@ -1,1 +1,12 @@
-hello
+array=[11,5,8,9,7]
+
+def selectionsort(array):
+  n = len(array) 
+  for i in range(n):
+    for j in range(0,n-i-1):
+      if array[j]>array[j+1]:
+        array[j], array[j+1]=array[j+1],array[j]
+    print(array)
+  return array
+
+print(selectionsort(array))
